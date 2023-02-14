350e49abe0be84c3a713828de2873bff20827ce5
Emilia Fabisiak <chojnowska.emilia@gmail.com>
2023-02-14 21:22:09 +0100

    added taggleBackground function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
-const welcome = () => {
-    console.log("Cześć! Witam wszystkich początkujących deweloperów.");
+{
+    const welcome = () => {
+        console.log("Cześć! Witam wszystkich początkujących deweloperów.");
+    }
+    
+    welcome();
+    
+    const taggleBackground = () => {
+        const body = document.querySelector(".body");
+        const nextColorName = document.querySelector(".js-nextColorName")
+
+        body.classList.toggle("body--grey");
+        nextColorName.innerText = body.classList.contains("body--grey") ? "białe" : "szare";
+    };
+    
+    
+    
+    let headerButton = document.querySelector(".js-button");
+    let headerImage = document.querySelector(".js-image");
+    let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
+
+    
+    headerButton.addEventListener("click", () => {
+        headerImage.remove();
+        headerButton.remove();
+    });
+    
+    changeBackgroundButton.addEventListener("click", taggleBackground);
+    
 }
-
-welcome();
-
-let header__button = document.querySelector(".js-button");
-let header__image = document.querySelector(".js-image");
-let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
-let body = document.querySelector(".body");
-let nextColorName = document.querySelector(".js-nextColorName")
-
-header__button.addEventListener("click", () => {
-    header__image.remove();
-    header__button.remove();
-});
-
-changeBackgroundButton.addEventListener("click", () => {
-    body.classList.toggle("body--grey");
-    nextColorName.innerText = body.classList.contains("body--grey") ? "białe" : "szare";
-});
